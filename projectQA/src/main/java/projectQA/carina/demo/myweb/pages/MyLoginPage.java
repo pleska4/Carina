9150ce6542df95044c080e26da2e4ec1684a5db7
Yauheni Pleskach <epleska4@gmail.com>
2022-06-30 16:18:19 +0300

    Signed-off-by: Yauheni Pleskach <epleska4@gmail.com>

--- projectQA/src/main/java/projectQA/carina/demo/myweb/pages/MyLoginPage.java
+++ projectQA/src/main/java/projectQA/carina/demo/myweb/pages/MyLoginPage.java
@@ -1,25 +1,19 @@
 package projectQA.carina.demo.myweb.pages;
 
-import java.lang.invoke.MethodHandles;
-import java.util.List;
-
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.R;
+import com.qaprosoft.carina.core.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
-import com.qaprosoft.carina.core.gui.AbstractPage;
-import org.testng.annotations.Test;
+import java.lang.invoke.MethodHandles;
 
-
-public class MyHomePage extends AbstractPage {
+public class MyLoginPage extends AbstractPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 
-    public MyHomePage(WebDriver driver) {
+    public MyLoginPage(WebDriver driver) {
         super(driver);
         setPageAbsoluteURL(R.CONFIG.get(Configuration.Parameter.URL.getKey()));
     }
--- projectQA/src/test/java/projectQA/carina/demo/onliner/HomeTest.java
+++ projectQA/src/test/java/projectQA/carina/demo/onliner/HomeTest.java
@@ -2,14 +2,20 @@ package projectQA.carina.demo.onliner;
 
 import com.ctc.wstx.shaded.msv_core.driver.textui.Debug;
 import com.qaprosoft.carina.core.foundation.AbstractTest;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import projectQA.carina.demo.mobile.gui.pages.common.LoginPageBase;
+import projectQA.carina.demo.mobile.gui.pages.ios.LoginPage;
 import projectQA.carina.demo.myweb.pages.MyHomePage;
+import projectQA.carina.demo.myweb.pages.MyLoginPage;
 
 
 public class HomeTest extends AbstractTest {
 
+    @FindBy(xpath = "//div[@class = header-style__enter']//a")
+    public ExtendedWebElement enterBtn;
 
     @Test
     public void openHomePage() {
@@ -19,7 +25,9 @@ public class HomeTest extends AbstractTest {
     }
 
     @Test
-    public void testEnterButton() {
-
+    public MyLoginPage testEnterButton() {
+        enterBtn.click();
+        return new MyLoginPage(getDriver()) {
+        };
     }
 }
